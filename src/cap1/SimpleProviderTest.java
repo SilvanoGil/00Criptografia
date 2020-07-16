89d98d2b53b51e3962be5b52904fb8c3d99bc819
sgil <sgil@pronoide.es>
2020-07-16 10:07:31 +0200

    Initial commit

---
+++ src/cap1/SimpleProviderTest.java
@@ -0,0 +1,27 @@
+package cap1;
+
+import java.security.Security;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+/**
+ * Basic class to confirm the Bouncy Castle provider is 
+ * installed.
+ */
+public class SimpleProviderTest
+{
+    public static void main(String[] args)
+    {
+    	//Security.addProvider(new BouncyCastleProvider());
+        String providerName = "BC";
+        
+        if (Security.getProvider(providerName) == null)
+        {
+            System.out.println(providerName + " provider not installed");
+        }
+        else
+        {
+            System.out.println(providerName + " is installed.");
+        }
+    }
+}
